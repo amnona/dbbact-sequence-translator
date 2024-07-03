9dbe38894eed25e15c2affe6759841425ab167f7
amnona <amnonim@gmail.com>
2024-07-03 13:23:47 +0300

    update from flask_autodoc to flask_selfdoc

--- dbbact_sequence_translator/autodoc.py
+++ dbbact_sequence_translator/autodoc.py
@@ -1,5 +1,6 @@
 # the autodoc variable for obtaining the REST API documentation for all user exposed APIs
 # from flask.ext.autodoc import Autodoc
-from flask_autodoc import Autodoc
+from flask_selfdoc import Autodoc
+# from flask_autodoc import Autodoc
 
 auto = Autodoc()
